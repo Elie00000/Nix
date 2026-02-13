61e8a409d07aa5024b77bf2dc0d8f9ea9f1b76bd
Jerome <133158597+Elie00000@users.noreply.github.com>
2026-02-13 14:21:04 +0100

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,72 +1,47 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 
 {
-  # === Paquets ===
+  # ==============================
+  # USER INFO
+  # ==============================
+  home.username = "elie";
+  home.homeDirectory = "/home/elie";
+  home.stateVersion = "26.05";
+
+  # ==============================
+  # PACKAGES
+  # ==============================
   home.packages = with pkgs; [
-    # Utils
-    lshw
-    picom
-    neofetch
-    feh
-    pywal
-    polybar
-    betterlockscreen
-    i3lock-color
-    mpv
-    xwinwrap
-    arandr
-    udiskie
-
-    # Apps
-    vscode
-    alsa-utils
-    shortwave
-    nekoray
-
-    # Terminals & Launchers
-    alacritty
-    rofi
+    lshw picom neofetch feh pywal polybar betterlockscreen
+    i3lock-color mpv xwinwrap arandr udiskie
+    vscode alsa-utils shortwave nekoray
+    alacritty rofi
 
     # Fonts
-    material-design-icons
-    noto-fonts
-    noto-fonts-color-emoji
-    font-awesome
-    lato
-    liberation_ttf
-    open-sans
-    roboto
-    ubuntu-classic
-    jetbrains-mono
-    monaspace
-    anonymousPro
-    fira-code
-    fira-code-symbols
+    material-design-icons noto-fonts noto-fonts-color-emoji
+    font-awesome lato liberation_ttf open-sans roboto
+    ubuntu-classic jetbrains-mono monaspace anonymousPro
+    fira-code fira-code-symbols
     nerd-fonts._0xproto
     nerd-fonts.droid-sans-mono
-    comic-neue
     nerd-fonts.space-mono
-    quicksand
+    comic-neue quicksand
   ];
 
-  # === Fonts ===
   fonts.fontconfig.enable = true;
 
-  # === Curseur ===
+  # ==============================
+  # CURSOR
+  # ==============================
   home.pointerCursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
     size = 24;
   };
 
-  # === Utilisateur ===
-  home = {
-    username = "elie";
-    homeDirectory = "/home/elie";
-    stateVersion = "26.05";
-  };
-
-  # === Zsh ===
+  # ==============================
+  # ZSH (user)
+  # ==============================
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -74,81 +49,58 @@
     syntaxHighlighting.enable = true;
   };
 
-  # === i3 ===
-  programs.i3 = {
-    enable = true;
-    package = pkgs.i3;
-    extraConfig = builtins.readFile ./dotfiles/i3/config;
-  };
-
-  # === Fichiers de config ===
+  # ==============================
+  # FILES
+  # ==============================
   home.file = {
-    # i3
     ".config/i3/config".source = ./dotfiles/i3/config;
-
-    # Polybar
     ".config/polybar/config.ini".source = ./dotfiles/polybar/config;
-
-    # Alacritty
-    ".config/alacritty/alacritty.yml".source = ./dotfiles/alacritty/alacritty.toml;
-
-    # Rofi
-    ".config/rofi/config.rasi".source = ./dotfiles/rofi/config.rasi;
-
-    # Betterlockscreen
-    ".config/betterlockscreen/betterlockscreenrc".source = ./dotfiles/betterlockscreen/betterlockscreenrc;
+    ".config/alacritty/alacritty.toml".source =
+      ./dotfiles/alacritty/alacritty.toml;
+    ".config/rofi/config.rasi".source =
+      ./dotfiles/rofi/config.rasi;
+    ".config/betterlockscreen/betterlockscreenrc".source =
+      ./dotfiles/betterlockscreen/betterlockscreenrc;
   };
 
-  # === Polybar via systemd user ===
+  # ==============================
+  # USER SERVICES
+  # ==============================
   systemd.user.services.polybar = {
     Unit = {
       Description = "Polybar";
-      Wants = [ "graphical-session.target" ];
       After = [ "graphical-session.target" ];
     };
     Service = {
-      Type = "simple";
-      ExecStart = "${pkgs.polybar}/bin/polybar -c ~/.config/polybar/config example";
+      ExecStart =
+        "${pkgs.polybar}/bin/polybar -c ~/.config/polybar/config.ini example";
       Restart = "always";
     };
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-    };
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 
-  # === Picom via systemd user ===
   systemd.user.services.picom = {
     Unit = {
-      Description = "Picom compositor";
-      Wants = [ "graphical-session.target" ];
+      Description = "Picom";
       After = [ "graphical-session.target" ];
     };
     Service = {
-      Type = "simple";
       ExecStart = "${pkgs.picom}/bin/picom";
       Restart = "always";
     };
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-    };
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 
-  # === Agent d'authentification polkit ===
   systemd.user.services.polkit-gnome-authentication-agent-1 = {
     Unit = {
-      Description = "polkit-gnome-authentication-agent-1";
-      Wants = [ "graphical-session.target" ];
+      Description = "Polkit Agent";
       After = [ "graphical-session.target" ];
     };
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-    };
     Service = {
-      Type = "simple";
-      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      ExecStart =
+        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
       Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
     };
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 }
